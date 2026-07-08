f2e367779b8fd374b76d3b3cfbd6087ea55c85a2
eogurlgurl-tech <eogurlgurl@gmail.com>
2026-07-08 13:22:36 +0900

    feat(core): add application constants

---
+++ js/constants.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * ============================================================
+ * Japanese Master Road
+ * Application Constants
+ * Version : 3.0.0
+ * ============================================================
+ */
+
+window.STORAGE_KEYS = Object.freeze({
+    PROGRESS: "jlpt_n4_progress",
+    STREAK: "jlpt_streak",
+    QUIZ_STATS: "jlpt_quiz_stats",
+    THEME: "theme"
+});
+
+window.THEME = Object.freeze({
+    LIGHT: "light",
+    DARK: "dark"
+});
+
+window.JLPT_LEVEL = Object.freeze({
+    N5: "N5",
+    N4: "N4",
+    N3: "N3",
+    N2: "N2",
+    N1: "N1"
+});
